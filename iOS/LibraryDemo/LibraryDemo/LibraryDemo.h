4547448a2cdd5a622584b9c884418ec9bc721fa0
Akira Nakajima <nakajima@altonotes.co.jp>
2020-06-08 13:43:48 +0900

    プロジェクトの作成

---
+++ iOS/LibraryDemo/LibraryDemo/LibraryDemo.h
@@ -0,0 +1,19 @@
+//
+//  LibraryDemo.h
+//  LibraryDemo
+//
+//  Created by Akira Nakajima on 2020/06/08.
+//  Copyright © 2020 Akira Nakajima. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LibraryDemo.
+FOUNDATION_EXPORT double LibraryDemoVersionNumber;
+
+//! Project version string for LibraryDemo.
+FOUNDATION_EXPORT const unsigned char LibraryDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibraryDemo/PublicHeader.h>
+
+
