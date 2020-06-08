bb94f875ae887b4acd94ffab53a6060c49287fc1
Akira Nakajima <nakajima@altonotes.co.jp>
2020-06-08 14:04:26 +0900

    Swiftlintの追加、サンプルプロジェクトの追加

---
+++ iOS/LibraryDemo/LibraryDemo/DemoUtils.swift
@@ -0,0 +1,15 @@
+//
+//  DemoUtils.swift
+//  LibraryDemo
+//
+//  Created by Akira Nakajima on 2020/06/08.
+//  Copyright © 2020 Akira Nakajima. All rights reserved.
+//
+
+import Foundation
+
+public class DemoUtils {
+    public static func demoMethod() {
+        print("@@@@ execute Demo Method!")
+    }
+}
---
+++ iOS/LibraryDemo/LibraryDemoSample/LibraryDemoSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LibraryDemoSample
+//
+//  Created by Akira Nakajima on 2020/06/08.
+//  Copyright © 2020 Akira Nakajima. All rights reserved.
+//
+
+import UIKit
+import LibraryDemo
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        DemoUtils.demoMethod()
+    }
+
+}
